9829753f8bee22a3be96bdeec34fe514cd2f0b82
Farheen Ansari <ansfarheen94@gmail.com>
2025-09-16 22:43:26 +0530

    16th September 2025 2nd Commit

---
+++ LearningATB14xJavaPrograms/src/ex_02_Java_Basics/Lab015_Variables.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics;
+
+public class Lab015_Variables {
+    public static void main(String[] args) {
+        byte age=65;
+        short age_pramod= 45;
+        System.out.println(age_pramod);
+    }
+}
---
+++ LearningATB14xJavaPrograms/src/ex_02_Java_Basics/Lab020_Interview_QA.java
@@ -0,0 +1,8 @@
+package ex_02_Java_Basics;
+
+public class Lab020_Interview_QA {
+    public static void main(String[] args) {
+        int Enum=10;
+        System.out.println(Enum);
+    }
+}
