8036a7e123c927e0299173a924bdced385d9a2ad
Farheen Ansari <ansfarheen94@gmail.com>
2025-09-16 20:57:22 +0530

    16th September 2025

---
+++ LearningATB14xJavaPrograms/src/ex_02_Java_Basics/Lab006_Comments_Multi.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+/**
+        *Author : Farheen
+        *Course : ATB14x
+        *Learning : Java
+ **/
+
+public class Lab006_Comments_Multi {
+    public static void main(String[] args) {
+        /*System.out.println("Hello World");*/
+    }
+}
---
+++ LearningATB14xJavaPrograms/src/ex_02_Java_Basics/Lab007_Comments.java
@@ -0,0 +1,12 @@
+package ex_02_Java_Basics;
+
+public class Lab007_Comments {
+
+    public static void main(String[] args) {
+        // Method Implementation
+
+//        System.out.println("1");
+
+//        System.out.println("2");
+    }
+}
